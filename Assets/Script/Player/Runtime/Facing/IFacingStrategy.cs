2911f0d25924517957a5fce0e0c2c5a5582cce78
Clam <wenlinchong17@gmail.com>
2026-03-12 23:00:15 +0800

    1

--- Assets/Script/Player/Runtime/Facing/IFacingStrategy.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IFacingStrategy
-{
-    void UpdateFacing(Player player, Vector2 input, float deltaTime);
-    void ForceFaceTarget(Player player, Vector2 targetPosition);
-}
