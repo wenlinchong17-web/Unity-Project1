5d78a23de78c0262c96aea1600db1b75d5c9864d
Clam <wenlinchong17@gmail.com>
2026-03-19 20:43:27 +0800

    1

--- Assets/Script/Player/Runtime/Movement/IMovementStrategy.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IMovementStrategy
-{
-    void Update(Player player, Rigidbody2D rb, Vector2 input, bool jumpHeld, bool jumpPressed, float deltaTime);
-}
